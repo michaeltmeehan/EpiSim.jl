c76265686f7cc257606804d803897006299fc777
michaeltmeehan <michael.meehan1@jcu.edu.au>
2025-04-13 21:19:21 +1000

    Removed old files

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,11 +7,3 @@ using RecursiveArrayTools
 using StatsBase
 using UnPack
 using Test
-
-
-# include("test_utils.jl")
-# include("test_outbreak.jl")
-# include("test_bdutils.jl")
-# include("test_crbd.jl")
-# include("test_mtbd.jl")
-# include("test_proc.jl")
