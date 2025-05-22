d9f5537437593b2131a1e229b1e10ba530057f38
michaeltmeehan <michael.meehan1@jcu.edu.au>
2025-05-22 10:13:18 +1000

    Pre-allocate simulation loggers

--- src/simulation/simulate.jl
+++ src/simulation/simulate.jl
@@ -35,17 +35,30 @@ end
 
 function simulate(rng::AbstractRNG,
                   model::AbstractModel;
-                  stop_condition::Function=get_default_stop_condition(model))
+                  stop_condition::Function=get_default_stop_condition(model),
+                  max_iter::Int=100_000)
 
     state = deepcopy(model.initial_state)
     event_types = model.event_types
-    event_log = initialize_event_log(state)
     event_rates = Vector{Float64}(undef, length(event_types))
 
-    state_log = [capture(state)]
+    initial_state_log = capture(state)
+    state_log = Vector{typeof(initial_state_log)}(undef, max_iter)
+    state_log[1] = initial_state_log
+
+    initial_event_log = initialize_event_log(state)
+    event_log = Vector{Union{event_types..., [typeof(event) for event in initial_event_log]...}}(undef, max_iter)
+    for (i, event) in enumerate(initial_event_log)
+        event_log[i] = event
+    end
+    
+    n = 1
+    m = length(initial_event_log)
+    # event_log = initialize_event_log(state)
+    # state_log = [capture(state)]
 
     # Main simulation loop
-    while !stop_condition(state)
+    while !stop_condition(state) && n < max_iter && m < max_iter
 
         update_event_rates!(event_rates, model.par, state)
 
@@ -62,18 +75,26 @@ function simulate(rng::AbstractRNG,
         # Update model state and extract concrete event record (e.g., Transmission(1, 2, 1.0))
         event = update_state!(rng, model.par, state, event_type)
 
+        # Update iteration counters
+        n += 1
+        m += 1
+
         # Update event log
-        push!(event_log, event)
+        event_log[m] = event
+        # push!(event_log, event)
 
         # Update state log
-        push!(state_log, capture(state))
+        state_log[n] = capture(state)
+        # push!(state_log, capture(state))
     end
-    return Outbreak(model, DataFrame(state_log), event_log)
+    return Outbreak(model, DataFrame(state_log[1:n]), event_log[1:m])
+    # return Outbreak(model, DataFrame(state_log), event_log)
 end
 
 
 function simulate(model::AbstractModel;
-                  stop_condition::Function=get_default_stop_condition(model))
+                  stop_condition::Function=get_default_stop_condition(model),
+                  max_iter::Int=100_000)
     return simulate(Random.GLOBAL_RNG, model; stop_condition=stop_condition)
 end
 
@@ -81,15 +102,16 @@ end
 function simulate(rng::AbstractRNG,
                   model::M,
                   n::Int;
-                  stop_condition::Function = get_default_stop_condition(model)) where M <: AbstractModel
+                  stop_condition::Function = get_default_stop_condition(model),
+                  max_iter::Int=100_000) where M <: AbstractModel
 
     if n == 1
-        return simulate(rng, model; stop_condition=stop_condition)
+        return simulate(rng, model; stop_condition=stop_condition, max_iter=max_iter)
     else
         seeds = rand(rng, UInt32(1):UInt32(2^31-1), n)
         replicates = Vector{Outbreak{M}}(undef, n)
         Threads.@threads for i in 1:n
-            replicates[i] = simulate(MersenneTwister(seeds[i]), model; stop_condition=stop_condition)
+            replicates[i] = simulate(MersenneTwister(seeds[i]), model; stop_condition=stop_condition, max_iter=max_iter)
         end
         return Ensemble{M}(model, replicates, seeds)
     end
@@ -98,6 +120,7 @@ end
 
 function simulate(model::AbstractModel, 
                   n::Int;
-                  stop_condition::Function = get_default_stop_condition(model))
-    return simulate(Random.GLOBAL_RNG, model, n; stop_condition=stop_condition)
+                  stop_condition::Function = get_default_stop_condition(model),
+                  max_iter::Int=100_000)
+    return simulate(Random.GLOBAL_RNG, model, n; stop_condition=stop_condition, max_iter=max_iter)
 end
